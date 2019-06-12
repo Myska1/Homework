de45ebfbbbe2da0d675e6d0ff1ad4e993af982c8
Michaela Cepáková <47025838+Myska1@users.noreply.github.com>
2019-06-12 14:34:13 +0200

    Update Test.py

--- Test.py
+++ Test.py
@@ -8,27 +8,27 @@ browser.get('https://www.alza.cz/')
 
 #selecting a link for Electro mobile cathegory
 filtr = browser.find_element_by_link_text('Elektromobilita').click()
-time.sleep(2)
+time.sleep(3)
 #selecting a link for Electro cars cathegory
 filtr1 = browser.find_element_by_link_text('Elektromobily').click()
-time.sleep(2)
+time.sleep(3)
 #filtr for setting up items from the most expensive thing
 filtr2 = browser.find_element_by_link_text('Od nejdražšího').click()
-time.sleep(2)
+time.sleep(3)
 #filter for choosing only items only in stock
 filtr3 = browser.find_element_by_xpath(".//*[contains(text(), 'Pouze skladem')]").click()
-time.sleep(2)
+time.sleep(3)
 #add the most expensive item into the cart
 buyMostExpensive = browser.find_element_by_link_text("Koupit").click()
-time.sleep(2)
+time.sleep(3)
 back = browser.find_element_by_link_text('Zpět ke zboží').click()
 time.sleep(3)
 #filtr for setting up items from the least expensive thing
 filtr4 = browser.find_element_by_link_text('Od nejlevnějšího').click()
-time.sleep(2)
+time.sleep(3)
 #add the least expensive item into the cart
 buyLastExpensive = browser.find_element_by_link_text("Koupit").click()
-time.sleep(2)
+time.sleep(3)
 #show me items in the basket
 filtr5 = browser.find_element_by_id('basket').click()
 time.sleep(5)
