1177b0c86f6cbc0e55626ccfe4d9b176d35bd1fe
Michaela Cepáková <47025838+Myska1@users.noreply.github.com>
2019-06-12 14:28:42 +0200

    Update Test.py

--- Test.py
+++ Test.py
@@ -6,26 +6,33 @@ from selenium.webdriver.common.keys import Keys
 browser = webdriver.Chrome(executable_path=r"C:\Users\pf932\Homework\chromedriver.exe")
 browser.get('https://www.alza.cz/')
 
+#selecting a link for Electro mobile cathegory
 filtr = browser.find_element_by_link_text('Elektromobilita').click()
 time.sleep(2)
+#selecting a link for Electro cars cathegory
 filtr1 = browser.find_element_by_link_text('Elektromobily').click()
 time.sleep(2)
-#filtr for set up from highest price to low
+#filtr for setting up items from the most expensive thing
 filtr2 = browser.find_element_by_link_text('Od nejdražšího').click()
 time.sleep(2)
-#filter for choose only items only in stock
+#filter for choosing only items only in stock
 filtr3 = browser.find_element_by_xpath(".//*[contains(text(), 'Pouze skladem')]").click()
 time.sleep(2)
+#add the most expensive item into the cart
 buyMostExpensive = browser.find_element_by_link_text("Koupit").click()
 time.sleep(2)
 back = browser.find_element_by_link_text('Zpět ke zboží').click()
 time.sleep(3)
+#filtr for setting up items from the least expensive thing
 filtr4 = browser.find_element_by_link_text('Od nejlevnějšího').click()
 time.sleep(2)
-buyLeastExpensive = browser.find_element_by_link_text("Koupit").click()
+#add the least expensive item into the cart
+buyLastExpensive = browser.find_element_by_link_text("Koupit").click()
 time.sleep(2)
+#show me items in the basket
 filtr5 = browser.find_element_by_id('basket').click()
 time.sleep(5)
+#printscreen
 browser.save_screenshot('basket.png')
 
 
