09819748c2035395102a49c9bfe14646bdfb9f87
Michaela Cepáková <47025838+Myska1@users.noreply.github.com>
2019-06-12 14:23:47 +0200

    Final version Test.py
    Open web page alza.cz and in category Electro cars select and add most expensive and the least expensive item into the shopping cart

--- Test.py
+++ Test.py
@@ -1,14 +1,32 @@
 import time
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
-driver = webdriver.Chrome(executable_path=r"C:\Users\pf932\Homework\chromedriver.exe")
-#driver = webdriver.Chrome('C:\Users\pf932\Homework')  # Optional argument, if not specified will search path.
-driver.get('http://www.google.com/xhtml');
-time.sleep(5) # Let the user actually see something!
-search_box = driver.find_element_by_name('q')
-search_box.send_keys('ChromeDriver')
-search_box.submit()
-time.sleep(5) # Let the user actually see something!
-driver.quit()
+browser = webdriver.Chrome(executable_path=r"C:\Users\pf932\Homework\chromedriver.exe")
+browser.get('https://www.alza.cz/')
 
+filtr = browser.find_element_by_link_text('Elektromobilita').click()
+time.sleep(2)
+filtr1 = browser.find_element_by_link_text('Elektromobily').click()
+time.sleep(2)
+#filtr for set up from highest price to low
+filtr2 = browser.find_element_by_link_text('Od nejdražšího').click()
+time.sleep(2)
+#filter for choose only items only in stock
+filtr3 = browser.find_element_by_xpath(".//*[contains(text(), 'Pouze skladem')]").click()
+time.sleep(2)
+buyMostExpensive = browser.find_element_by_link_text("Koupit").click()
+time.sleep(2)
+back = browser.find_element_by_link_text('Zpět ke zboží').click()
+time.sleep(3)
+filtr4 = browser.find_element_by_link_text('Od nejlevnějšího').click()
+time.sleep(2)
+buyLeastExpensive = browser.find_element_by_link_text("Koupit").click()
+time.sleep(2)
+filtr5 = browser.find_element_by_id('basket').click()
+time.sleep(5)
+browser.save_screenshot('basket.png')
+
+
+browser.quit()
