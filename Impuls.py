9e5281fa7bfe240194f98c5e84f50199340a4d40
Michaela Cepáková <michaela.cepakova@gmail.com>
2019-06-14 09:16:30 +0200

    Job pro sestricku na impuls soutez

--- Impuls.py
+++ Impuls.py
@@ -2,3 +2,32 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+browser = webdriver.Chrome(executable_path=r"/Users/myska/Documents/Python/Homework/chromedriver")
+browser.get('http://www.impuls.cz/souteze/hti-milionovy-zavod/')
+
+email = browser.find_element_by_id("email")
+email.send_keys("lucinka.cepakova@gmail.com")
+
+name = browser.find_element_by_id("name1")
+name.send_keys("Lucie")
+
+surname = browser.find_element_by_id("name2")
+surname.send_keys("Cepáková")
+
+age = browser.find_element_by_id("age")
+age.send_keys("33")
+
+zipcode = browser.find_element_by_id("zipcode")
+zipcode.send_keys("19000")
+
+browser.find_element_by_xpath("//select[@name='pohlavi']/option[text()='Žena']").click()
+
+phone = browser.find_element_by_id("phone")
+phone.send_keys("773641618")
+
+filtr5 = browser.find_element_by_id('submit').click()
+
+time.sleep(15)
+browser.quit()
+
+
